a28e19d1ca3ad5934c88e3304c486204e33a54a7
passosleo <leonardopassos98@gmail.com>
2024-07-28 21:49:10 -0300

    chore: remove dist folder

--- dist/src/infra/http/schemas/sign-up/send-verify-email-schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SendVerifyEmailSchema = void 0;
-const zod_1 = require("zod");
-exports.SendVerifyEmailSchema = {
-    body: {
-        email: zod_1.z.string().email().max(255).trim().toLowerCase(),
-    },
-};
--- dist/src/infra/http/schemas/user/search-user-schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SearchUserSchema = void 0;
-const zod_1 = require("zod");
-exports.SearchUserSchema = {
-    query: {
-        username: zod_1.z.string().min(3).max(255),
-    },
-};
